e2615bcec7d044b9ab3e710f51c8da5e0e6618b0
Kevin Brent <kevin@kevinbrent.com>
2021-10-19 16:08:57 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# WP Data Sync - Custom Parent term
+
+## Description ##
+
+Add a custom parent term to an existing parent term.
+
+### Helpful Links ###
+
+* [WP Data Sync](https://wpdatasync.com "WP Data Sync")
+* [WP Data Sync Blog](https://wpdatasync.com/blog/ "WP Data Sync Blog")
+* [Developer Documentation](https://wpdatasync.com/docunentation/ "Developer Documentation")
+
+__[Change Log](https://wpdatasync.com/changelog/wp-data-sync-1/ "Change Log")__
---
+++ wp-data-sync.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: WP Data Sync - Custom Parent Term
+ * Plugin URI:  https://wpdatasync.com/products/
+ * Description: Add a custom parent term to existing parent terms.
+ * Version:     1.0.0
+ * Author:      WP Data Sync
+ * Author URI:  https://wpdatasync.com
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: wp-data-sync
+ *
+ * Package:     WP_DataSync
+*/
+
+namespace WP_DataSync\App;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+define( 'WPDSYNC_TEMPLATE_VERSION', '1.0.0' );
+
